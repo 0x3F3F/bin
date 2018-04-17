203c3131954b34014bf5f69d8f26019dfe7d0854
0x3F3F <26686669+0x3F3F@users.noreply.github.com>
2018-04-17 11:25:24 +0200

    Script to play podcasts via Newsboat

---
+++ NewsboatPodPlay.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Play podcasts with mpv.  First switching to Main workspace.
+
+# Switch to web workspace
+i3-msg "workspace 1:Web "
+
+# Deal with handelling of specific sites:
+
+if [[ $1 == *"youtube"* ]]
+then
+	# If youtube try and select lower quality as it's only a wee window
+	echo "Playing: Youtube"
+	mpv --really-quiet --osd-level=1 --ytdl-format="bestvideo[height<=?480][vcodec!=vp9]+bestaudio/best" "$1"
+
+elif [[ $1 == *"investorfieldguide"* || $1 == *"rationallyspeaking"* ]]
+then
+	# Issue with these site as links to page not podcast url.  Open website
+	echo "Opening with firefox"
+	firefox "$1"
+
+else
+	# This is probably MP3, force it to open a window
+	echo "Playing: MP3"
+	mpv --force-window --no-video --osd-level=3 "$1"
+fi
+
+# Can't get it to switch back, not huge issue.
+#i3-msg "workspace 3:Bash "
+
