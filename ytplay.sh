c39b3985ad548f4474f04a480dd9759649fd841c
0x3F3F <26686669+0x3F3F@users.noreply.github.com>
2018-10-02 01:25:42 +0200

    Switch ytplay to use omxplayer as has hardware acceleration

--- ytplay.sh
+++ ytplay.sh
@@ -8,11 +8,16 @@
 # https://nwgat.ninja/quick-easy-compiling-mpv-for-raspberry-pi/
 # https://raspberrypi.stackexchange.com/questions/57847/no-opengl-hardware-rendering-using-mpv
 # This didn't work, hit issue when compiling ffmpeg.  
-# Re-installed ffmpeg/mpv fropm repo, stick to 480p for now as that plays without issues. sometimes 720p too.
-mpv  --ytdl-format="bestvideo[vcodec!=vp9][height<=?720][fps<=30]+bestaudio[ext=m4a]" "$1"
-
-# OMX does play if supply video stream url so can handle https. it must use ffmpeg to multiplex audio
-# focus on installing ffmpeg with openssl and then switch to omx
+# Re-installed ffmpeg/mpv from repo, stick to 480p for now as that plays without issues. sometimes 720p too.
+#mpv  --quiet --osd-level=1 --volume-max=130 --volume=115 --autofit="100%x100%" --ytdl-format="bestvideo[vcodec!=vp9][height<=?720][fps<=30]+bestaudio[ext=m4a]" "$1"
 
 
+# Was having issue with vids with no sounds.  
+# If specify diff audio/video streams they're not being multiplexed (ffmpeg issue?)
+# Use old youtube-dl behaviour specifying only quality in single file - See github page.
+# Try for mp4 first (so wont get vp9).  If no mp4 then default to best that is avilable.
+#omxplayer $(youtube-dl -g -f best "$1")
+sudo sh -c "TERM=linux setterm  -foreground black -clear all >/dev/tty0"		# Clear terminal whoch xould be visible
+omxplayer $(youtube-dl -g -f 'mp4[height <=? 720]/best[height <=? 720]' "$1")
+sudo sh -c "TERM=linux setterm  -foreground white -clear all >/dev/tty0"		# Restore normal terminal colour
 
