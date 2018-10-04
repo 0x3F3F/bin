2130761ce75b30b8e7f39f887b7204c7675ab0ec
0x3F3F <26686669+0x3F3F@users.noreply.github.com>
2018-10-05 01:46:55 +0200

    Added playlist functionality with url fix

--- ytplay.sh
+++ ytplay.sh
@@ -17,6 +17,12 @@
 #				VID=$1
 #				ssh pi@raspberrypi -t "/home/pi/bin/ytplay.sh $VID"
 #
+#		Dependencies:
+#				omxplayer
+#				youtube-dl
+#				jq
+#				A Raspberry Pi :)
+#
 ############################################################################################## 
 
 PlayVid()
@@ -35,35 +41,68 @@ PlayVid()
 # Clear terminal which could be visible at sides of video
 sudo sh -c "TERM=linux setterm  -foreground black -clear all >/dev/tty0"	
 
-# Kill existing omxplayer that might be runnning.  Can only watch one vid at a time!
+# Kill existing omxplayer/feh instances.
 if pgrep -x "omxplayer" > /dev/null
 then
-	killall omxplayer
+	killall omxplayer 
 fi
 
+# fbi processes may be running. kill 'em
+sudo killall fbi
 
 # Determine if playing single video or playlist
 if [[ $1 = *"list="* ]]; then
 
-	# If playlist download it as json, use jq to get list of id's
-	PL_IDS=$(youtube-dl -j --flat-playlist "$1" | jq -r '.id')
+	# Issue with url containing playlist not working.  fix it
+	FIXED=$(echo "$1"|sed 's/watch.\+list=/playlist?list=/g')
 
-	# Parse line at a time
-	for line in $PL_IDS
+	# If playlist download with youtube-dl it as json
+	PLAYLIST_JSON=$(youtube-dl -j --flat-playlist "$FIXED" )
+	
+	# All on same line.  Split onto separate for easy processing. Note \n doesn't work.
+	PLAYLIST_JSON=$(echo $PLAYLIST_JSON | sed 's/} {/}\r{/g')
+
+	# For loop uses whitespace.  Use Internal File Separator to use \r instead
+	IFS=$'\r'
+	RED='\033[0;31m'
+	NO_COL='\033[0m'
+	for line in $PLAYLIST_JSON
 	do
+		# Get the Title and Url.  Use sq ro process json
+		TITLE=$(echo $line | jq -r '.title')
+		URL=$(echo $line | jq -r '.url')
+
+		# Output what we're playing to terminal
+		clear
+		#echo $TITLE
+		#echo $URL
+		echo -e "Playing: ${RED}$TITLE${NO_COL}"
+
 		# Create Url from id and play using our function above
-		PlayVid "https://www.youtube.com/watch?v=$line"
+		PlayVid "https://www.youtube.com/watch?v=$URL"
+
+		# Once video terminate, give user ability to quit out of playlist
+		read -p "Press q to quit playlist " -t 2 -n 1 key <&1
+		if [[ $key = q ]] ; then
+			break
+		fi
+
 	done
 
 else
 
-	# Playing Single Video
+	# Easy, Playing Single Video
 	PlayVid "$1"
 
 fi
 
 
-
 # Restore normal terminal colours
 sudo sh -c "TERM=linux setterm  -foreground white -clear all >/dev/tty0"	
+
+
+# Actually, lets display something instead of a black screen
+# X not running so use framebuffer. Neaeds sudo as virit console owned by root
+PHOTODIR=~/Pictures/Wallpaper/Rasp 
+sudo fbi -a --noverbose -T 1 -t 80 -u `find $PHOTODIR -iname "*.jpg"`
 
