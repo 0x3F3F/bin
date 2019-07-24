3a845df65f7173d12ed58e281ceacfb955641a09
0x3F3F <26686669+0x3F3F@users.noreply.github.com>
2019-07-24 22:03:55 +0200

    Script to update mps-youtube and youtube-dl

---
+++ ytupdate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Update youtube-dl and mps-youtube
+pip3 install --user --upgrade youtube-dl
+pip3 install --user --upgrade mps-youtube
+
+# Update pip, to stop crap erros
+pip install --user --upgrade pip
+
