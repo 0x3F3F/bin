08d9dc59d8fc197a2be2dba07f551bf7b90dae45
iain <26686669+0x3F3F@users.noreply.github.com>
2026-09-24 22:59:45 +0100

    shortcut to update Optiplex2 ip address

---
+++ updateip.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This command update the ip address in Op2Config with the supplied ip
+# Usage
+# In .bashrc add:
+# u () { $HOME/bin/updateip.sh $1 ; }
+
+sed -r -i 's/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b'/$1/ ~/.ssh/Op2Config
+
